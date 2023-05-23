5341ce777ef79496bd4d0e3abba9007922b7fabc
vitordsg <vitinho6.6@hotmail.com>
2023-05-23 18:58:42 -0300

    exercicios

---
+++ exercicios/aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 65
+console.log(`A Velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) { // Condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Digija sempre usando cinto de segurança`)
