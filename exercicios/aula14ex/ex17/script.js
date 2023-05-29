f76dd429a0860de1b7b9f2f1ef37513cbf088c65
vitordsg <vitinho6.6@hotmail.com>
2023-05-29 02:00:53 -0300

    ex17 tabuada

--- exercicios/aula14ex/ex17/script.js
+++ exercicios/aula14ex/ex17/script.js
@@ -1,1 +1,25 @@
-alert('Olá')
+function tabuada() {
+    let numero = document.getElementById('txtnumero')
+    let tabuada = document.getElementById('seltabuada')
+
+    if(numero.value.length == 0){
+
+        alert('[ERRO] Digite um número!')
+    }
+
+    else {
+
+        let n = Number(numero.value)
+        let c = 1
+        tabuada.innerHTML = ''
+
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n *c}`
+            item.value = `tab${c}`
+            tabuada.appendChild(item)
+            c++
+        } 
+    }
+    
+}
