5ba4677fe98aebb5a47008bd5b0dd6ddd28dd40e
vitordsg <vitinho6.6@hotmail.com>
2023-05-28 23:41:21 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,33 @@
+/* DO WHILE / TESTE LOGICO NO FINAL */
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 10)
+
+
+
+
+/* WHILE / TESTE LOGICO NO INICIO
+
+var c = 1
+while ( c <= 10 ) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+
+
+*/
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
