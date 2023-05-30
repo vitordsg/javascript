377a197b1c6baad85518536372bcd2a6e4a9ca92
vitordsg <vitinho6.6@hotmail.com>
2023-05-30 04:57:08 -0300

    att

---
+++ exercicios/aula16ex/ex18/script.js
@@ -0,0 +1,36 @@
+let num = document.querySelector('input#txtnumero')
+let listas = document.querySelector('select#lista')
+let res = document.querySelector('div#resultado')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    }
+
+    else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    }
+
+    else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        alert('CERTO')
+    }
+
+    else {
+        window.alert('Valor Invalido ou já inserido na lista')
+    }
+
+
+}
